64eecaba7d7766c1c5e2a5218244954cf73728ac
Stefano <tepo@hotmail.it>
2023-07-15 11:38:45 +0200

    add hero to homepage

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -5,7 +5,31 @@ import heroBcg from '../assets/hero-bcg.jpeg'
 import heroBcg2 from '../assets/hero-bcg-2.jpeg'
 
 const Hero = () => {
-  return <h4>hero</h4>
+  return (
+    <Wrapper className="section-center">
+      <article className="content">
+        <h1>
+          design your <br />
+          comfort zone
+        </h1>
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris
+          sodales odio ut lectus tristique, quis venenatis ligula scelerisque.
+          Morbi sit amet velit vitae tellus tincidunt maximus. Curabitur neque
+          nisi, convallis ac magna eget, pretium vulputate lacus. Sed eget diam
+          sit amet felis feugiat pellentesque eu vitae tellus. Nullam elit
+          ipsum.
+        </p>
+        <Link to='/products' className='btn hero-btn'>
+          shop now
+        </Link>
+      </article>
+      <article className='img-container'>
+        <img src={heroBcg} alt='nice table' className='main-img'/>
+        <img src={heroBcg2} alt='person working' className='accent-img'/>
+      </article>
+    </Wrapper>
+  );
 }
 
 const Wrapper = styled.section`
