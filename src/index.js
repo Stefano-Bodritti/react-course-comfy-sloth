e95fdf7d6500649dedab798e9930d0a482074668
Stefano <tepo@hotmail.it>
2023-08-04 13:04:34 +0200

    setting up auth0 authentication

--- src/index.js
+++ src/index.js
@@ -11,12 +11,25 @@ import { Auth0Provider } from '@auth0/auth0-react';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
+// dev-qe6885nx0x8vdajv.us.auth0.com
+// nOjXiIHxUAZBYtyJ1g9yhzaiI6hWJ4ZU
+
 root.render(
-  <ProductsProvider>
-    <FilterProvider>
-      <CartProvider>
-        <App />
-      </CartProvider>
-    </FilterProvider>
-  </ProductsProvider>
+  <Auth0Provider
+    domain="dev-qe6885nx0x8vdajv.us.auth0.com"
+    clientId="nOjXiIHxUAZBYtyJ1g9yhzaiI6hWJ4ZU"
+    authorizationParams={{
+      redirect_uri: window.location.origin,
+    }}
+  >
+    <UserProvider>
+      <ProductsProvider>
+        <FilterProvider>
+          <CartProvider>
+            <App />
+          </CartProvider>
+        </FilterProvider>
+      </ProductsProvider>
+    </UserProvider>
+  </Auth0Provider>
 );
